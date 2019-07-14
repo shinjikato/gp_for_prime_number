207425adf8ccd56fa6edc32d9b4bf56097fb8596
shinjikato <nijishow@gmail.com>
2019-07-14 20:20:55 +0900

    make files

---
+++ gp.py
@@ -0,0 +1,16 @@
+
+class GPRegressor:
+	def __init__(self):
+		"no wirte"
+	def fit(X,y):
+		"no wirte"
+	def predict(X):
+		"no write"
+	def score(X,y):
+		"no write"
+
+	def get_params(self, deep=True):
+		return {}
+
+	def set_params(self, **parameters):
+		"no write"
