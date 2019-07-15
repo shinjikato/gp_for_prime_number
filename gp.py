4948fa9f0c6e6e0cdc0a7cf28a59f87f1b63cf87
shinjikato <nijishow@gmail.com>
2019-07-15 20:23:36 +0900

    define main function(test code)

--- gp.py
+++ gp.py
@@ -1,9 +1,11 @@
 import numpy as np
 import base
+import prime_number
+import random
 
 class GPRegressor:
 	def __init__(self, max_generation=200, pop_size = 500,
-		tour_size=64, batch_size=8, CXPB=0.8, MUTPB=0.1, limit=18):
+		tour_size=64, batch_size=8, CXPB=0.8, MUTPB=0.1, limit=18, printlog=False):
 		self.max_generation = max_generation
 		self.pop_size = pop_size
 		self.tour_size = tour_size
@@ -11,13 +13,24 @@ class GPRegressor:
 		self.CXPB = CXPB
 		self.MUTPB = MUTPB
 		self.limit = limit
-	def fit(X,y):
-		self.leafSet, self.nodeSet = base.makeNodeSet()
+		self.printlog = printlog
+	def geneinfo(self,pop,g):
+		fits = [tree.fitness for tree in pop if tree.fitness != None]
+		max_fit = max(fits)
+		min_fit = min(fits)
+		ave_fit = sum(fits)/len(fits)
+		return "{:0=6} : max {:.6f}, ave {:.6f}, min {:.6f}".format(g,max_fit,ave_fit,min_fit)
+
+	def fit(self, X, y):
+		self.nodeSet, self.leafSet  = base.makeNodeSet()
 		pop = base.initial_population(self.pop_size, self.nodeSet, self.leafSet)
 		elite = None
 		for g in range(self.max_generation):
 			pop = base.evaluation(pop, X, y)
-			pop = base.selection(pop, elite, batch_size=8, tour_size=64)
+			if self.printlog: print(self.geneinfo(pop,g))
+			pop,elite = base.selection(pop, elite, batch_size=8, tour_size=64)
+
+
 			pop = base.crossover(pop, self.CXPB, self.limit)
 			pop = base.mutation(pop, self.MUTPB, self.nodeSet, self.leafSet)
 		pop = base.evaluation(pop, X, y)
@@ -26,11 +39,11 @@ class GPRegressor:
 		self.tree = elite
 		return self
 
-	def predict(X):
+	def predict(self, X):
 		return self.tree.run(X)
-	def score(X,y):
+	def score(self, X, y):
 		_y = self.predict(X)
-		s = np.sum((y-_y)**2)/len(y)
+		s = np.sum(np.abs(y-_y))/len(y)
 		return -1*s
 
 	def get_params(self, deep=True):
@@ -40,9 +53,47 @@ class GPRegressor:
 				"batch_size":self.batch_size,
 				"CXPB":self.CXPB,
 				"MUTPB":self.MUTPB,
-				"limit":self.limit}
+				"limit":self.limit,
+				"printlog":self.printlog}
 
 	def set_params(self, **parameters):
 		for parameter, value in parameters.items():
 			setattr(self.params,parameter, value)
 		return self
+
+if __name__ == "__main__":# test code
+	random.seed(0)
+	np.random.seed(0)
+	N = 10000
+
+	X, y = prime_number.create(N=int(N/5),start_index=1)
+	test_X, test_y = prime_number.create(N=N,start_index=1)
+	model = GPRegressor(pop_size=1000,printlog=True,max_generation=1000)
+	model.fit(X, y)
+	print(model.tree.toTexText())
+	print(model.score(X, y))
+	print(model.score(test_X, test_y))
+
+	from sympy import sympify, simplify, latex
+	f = sympify(model.tree.toSympyInputText())
+	print(latex(f))
+	f_ = simplify(f)
+	print(latex(f_))
+
+	import matplotlib.pyplot as plt
+	x = test_X.flatten()
+	plt.plot(x,test_y,label="true prime number")
+	_y = model.predict(test_X)
+	plt.plot(x,_y,label="test  predict")
+	_y = model.predict(X)
+	x = X.flatten()
+	plt.plot(x,_y,label="train predict")
+	plt.legend(loc="upper left")
+	#plt.xticks(np.arange(1,))
+	plt.xlim([1,N])
+	plt.xlabel("x = index")
+	plt.ylabel("y = f(x) : f is prime number function")
+	plt.savefig("predict_graph.pdf")
+
+
+
